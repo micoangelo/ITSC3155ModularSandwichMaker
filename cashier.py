7570d877316c8f379f6f983835b9da6775c6912f
Mico <mico.masangkay02@gmail.com>
2024-09-26 19:07:09 -0400

    Added Cashier class with process_coins and transaction_result methods

--- cashier.py
+++ cashier.py
@@ -11,3 +11,26 @@ class Cashier:
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
         ##
+class Cashier:
+    def __init__(self):
+        pass
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        quarters = int(input("How many quarters?: ")) * 0.25
+        dimes = int(input("How many dimes?: ")) * 0.10
+        nickels = int(input("How many nickels?: ")) * 0.05
+        pennies = int(input("How many pennies?: ")) * 0.01
+        total = quarters + dimes + nickels + pennies
+        return total
+
+    def transaction_result(self, coins, cost):
+        """Return True when the payment is accepted, or False if money is insufficient."""
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            print(f"Transaction successful. Change: ${change}")
+            return True
+        else:
+            print("Sorry, that's not enough money. Transaction failed.")
+            return False
